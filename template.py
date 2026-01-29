495e7ddfb47ff81a664f10edb9e22c69cc21331b
saba shahbaz <sabashahbaz731@gmail.com>
2026-01-29 11:31:15 +0500

    readme file updated

---
+++ template.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+
+project_name = "Us_Visa"
+list_of_files = [
+    Path(f"{project_name}/__init__.py"),
+    Path(f"{project_name}/components/__init__.py"),
+    Path(f"{project_name}/components/data_ingestion.py"),
+    Path(f"{project_name}/components/data_validation.py"),
+    Path(f"{project_name}/components/data_transformation.py"),
+    Path(f"{project_name}/components/model_trainer.py"),
+    Path(f"{project_name}/components/model_evaluation.py"),
+    Path(f"{project_name}/components/model_pusher.py"),
+    Path(f"{project_name}/configuration/__init__.py"),
+    Path(f"{project_name}/constants/__init__.py"),
+    Path(f"{project_name}/entity/__init__.py"),
+    Path(f"{project_name}/entity/config_entity.py"),
+    Path(f"{project_name}/entity/artifact_entity.py"),
+    Path(f"{project_name}/exception/__init__.py"),
+    Path(f"{project_name}/logger/__init__.py"),
+    Path(f"{project_name}/pipline/__init__.py"),
+    Path(f"{project_name}/pipline/training_pipeline.py"),
+    Path(f"{project_name}/pipline/prediction_pipeline.py"),
+    Path(f"{project_name}/utils/__init__.py"),
+    Path(f"{project_name}/utils/main_utils.py"),
+    Path("app.py"),
+    Path("requirements.txt"),
+    Path("Dockerfile"),
+    Path(".dockerignore"),
+    Path("demo.py"),
+    Path("setup.py"),
+    Path("config/model.yaml"),
+    Path("config/schema.yaml"),
+]
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+    else:
+        print(f"file is already present at: {filepath}")
+
