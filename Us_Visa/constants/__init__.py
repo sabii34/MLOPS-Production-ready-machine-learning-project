855658c8187cd0cd9101b10ebcf7e1c18e097e15
saba shahbaz <sabashahbaz731@gmail.com>
2026-01-31 18:36:19 +0500

    init_.py added

---
+++ Us_Visa/constants/__init__.py
@@ -0,0 +1,14 @@
+import os
+from datetime import date
+
+DB_NAME="US_VISA"
+
+COLLECTION_NAME="visa_dataset"
+
+CONNECTION_URL="mongodb+srv://sabashahbaz731_db_user:uWSV0cgHHFf5bUTq@cluster0.k7889eq.mongodb.net/?appName=Cluster0"
+
+PIPLINE_NAME: str = "us_visa_pipeline"
+ARTIFACTS_DIR: str = "artifacts"
+
+MODEL_FILE_NAME: str = "model.pkl"
+
